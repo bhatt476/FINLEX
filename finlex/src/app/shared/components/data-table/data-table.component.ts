8aa75153ed4f2f02367db9fac765888f45bb2b30
kartik verma <kartik@metadesignsolutions.co.uk>
2020-09-03 12:02:41 +0530

    Added details button

--- finlex/src/app/shared/components/data-table/data-table.component.ts
+++ finlex/src/app/shared/components/data-table/data-table.component.ts
@@ -10,27 +10,40 @@ export class DataTableComponent implements OnInit  {
   options = {};
   data = [];
   columns: any = {};
+  @ViewChild('buttonsTemplate') buttonsTemplate: TemplateRef<any>;
 
   optionsBasicNoData = {};
   dataBasicNoData = [];
   columnsBasicNoData: any = {};
-
+  headers;
   @Input() tableData: any;
   constructor(private appService: MainService) { }
   ngOnInit(): void {
-    let headers;
     if (this.tableData.length && this.tableData[0]){
-      headers = Object.keys(this.tableData[0]);
+      this.headers = Object.keys(this.tableData[0]);
     }
-    this.columns = headers.map((header) => {
+    this.columns = this.headers.map((header) => {
       return {
         title: this.capitalizeFirstLetter(header),
         key: header,
+        width: 50
       };
     });
   }
 
+  ngAfterViewInit(){
+    setTimeout(() => {
+      this.columns.push({title: 'Actions', cellTemplate: this.buttonsTemplate});
+      this.columns = this.columns.splice(0, this.columns.length);
+    });
+
+  }
+
   capitalizeFirstLetter(text: string) {
     return text.charAt(0).toUpperCase() + text.slice(1);
+  }
+
+  viewDetails(event){
+    console.log('event is', event);
   }
 }
